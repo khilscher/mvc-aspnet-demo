34bb76fffd791f987e75bb4f7cc508b2202caf03
khilscher <kevin@nospam.ca>
2016-01-12 16:29:41 -0700

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvc_aspnet.Startup))]
+namespace mvc_aspnet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
